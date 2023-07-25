20a13c4bac25f1bff0c566dbec96fb8c24bcd9a3
minsoda <133620287+minsoda@users.noreply.github.com>
2023-07-25 17:41:02 +0900

    .

--- 3_Javascript/js/실습문제2.js
+++ 3_Javascript/js/실습문제2.js
@@ -1,4 +1,4 @@
-console.log("!!");
+console.log("!!"); 
 let maxScrollvalue = document.body.offsetHeight - window.innerHeight;
 const progressBar = document.querySelector('.progress-bar');
 
@@ -28,3 +28,73 @@ window.addEventListener('scroll', function(){
 window.addEventListener('resize', resizeHandler);
 // 처음에 호출되던게 한번더 호출이 되어야하니까 밖에 호출
 resizeHandler();
+
+// navigation click event
+const nav = document.querySelector('nav');
+// 현재 선택 변수 설정
+let current;
+
+function navHandler(e) {
+    // nav를 건들였을때 적용되는 스타일 방지 조건
+    if(e.target !== e.currentTarget){
+    console.log(e.target);
+    e.target.style.backgroundColor = 'black';
+    e.target.style.color = 'white';
+    }
+
+    // 클릭했을 현재 조건 
+    if(current){
+        current.style.backgroundColor = 'transparent';
+        current.style.color = 'black';
+    }
+
+    // 현재를 담아야함!
+    current = e.target;
+}
+
+nav.addEventListener('click', navHandler);
+
+// mouse wheel event
+// 휠 눌릴때마다 넘어가는 구간별로 필요
+const section1 = document.querySelector('#section1');
+const section2 = document.querySelector('#section2');
+const section3 = document.querySelector('#section3');
+
+// 위에서부터 가는걸로 section1 명시
+let currentSection = section1;
+
+window.addEventListener('wheel', function(event){
+    if(event.deltaY > 0){ // 휠을 아래로 내린 경우
+        if(currentSection === section1) {
+            window.scrollTo({top: section2.offsetTop, behavior: 'smooth'});
+            currentSection = section2;
+
+        } else if(currentSection === section2){
+            window.scrollTo({top: section3.offsetTop, behavior: 'smooth'});
+            currentSection = section3;
+        }
+    } else { // 휠을 위로 올린 경우
+        if(currentSection === section3){
+            window.scrollTo({top: section2.offsetTop, behavior: 'smooth'});
+            currentSection = section2;
+
+        }else if(currentSection == section2){
+            window.scrollTo({top: section1.offsetTop, behavior: 'smooth'});
+            currentSection = section1;
+        }
+    }
+
+});
+
+// scroll - animation 살짝
+window.addEventListener('scroll', function(){
+    // top처럼 고정이 아니라서 휠로 움직였을 경우 0으로 감
+    console.log(section2.getBoundingClientRect().top);
+
+    if(section2.getBoundingClientRect().top === 0){
+        console.log(section2.children[0]);
+        section2.children[0].classList.add('text-animation');
+    }else {
+        section2.children[0].classList.remove('text-animation');
+    }
+});

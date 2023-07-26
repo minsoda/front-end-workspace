15fbb2212cdf37aeb7b7268970c4307eaf5e939a
minsoda <133620287+minsoda@users.noreply.github.com>
2023-07-26 15:47:40 +0900

    .

---
+++ 4_jQuery/js/01_선택자.js
@@ -0,0 +1,129 @@
+// 축약버전
+$(function(){
+    // 1. 태그 선택자
+    // 자바스크립트 방식
+    const pList = document.querySelectorAll('p');
+    for(p of pList) {
+        console.log(p);
+        p.style.color = "white";
+        p.style.backgroundColor ="tomato";
+    }
+
+    // 제이쿼리 방식. 계속 연결 가능.
+    $('h5').css('color', 'skyblue').css('background-color', 'red');
+
+    // 2. 아이디 선택자
+    // 자바스크립트 방식
+    document.querySelector('#id1').style.color = 'crimson';
+
+    // 제이쿼리 방식
+    $('#id2').css('color', 'yellowgreen');
+
+    // 3. 클래스 선택자
+    // 자바스크립트 방식
+    const items = document.querySelectorAll('.item');
+    for(it of items){
+        it.style.color ='tomato';
+    }
+
+    // 제이쿼리 방식
+    // 한줄도 가능하지만 중괄호안에 넣어서도 가능
+    $('.item').css({
+        'color': 'white',
+        'background-color': 'indigo'
+    });
+
+    // 4. 자식 선택자와 자손 선택자
+    // 1) 자식 선택자 : 부모 > 자식
+    $('div > h3').css('color', 'violet');
+    $('div > ul > li').css('color', 'green');
+    $('div > ul > li > h3').css('color', 'indianred');
+
+    // 2) 자손 선택자 : 부모 자손
+    $('div h3').css('background-color', 'khaki');
+    $('div .ch').css('background-color', 'lime');
+
+    // 5. 속성 선택자
+    $('#div1 input[class]').css('background-color', 'turquoise');
+    $('#div1 input[type=text]').val('ChangeValue');
+    $('#div1 input[class~=test1]').val('123456789');
+    $('#div1 input[type^=ra]').prop('checked', true);
+    $('#div1 input[type$=box]').prop('checked', true);
+    $('#div1 input[class*=st3]').css({
+        'width': '100px', 'height': '100px'
+    }).val('10000');
+
+    // 6. 입력 양식 선택자
+    $('#div2 :text').css('background-color', 'pink');
+    // 글자 : 버튼, 사이즈 : 100px, 100px
+    $('#div2 :button').css({
+        'width': '100px', 'height': '100px'
+    }).val('버튼');
+    // 체크박스 - 체크된 상태, 사이즈 : 50px, 50px
+    $('#div2 :checkbox').prop('checked', true).css({
+        'width':'50px', 'height': '50px'
+    })
+    // 파일-배경색상 : yellow
+    $('#div2 :file').css('background-color', 'yellow');
+
+    // 7. 입력 양식 상태에 대한 선택자
+    // 1) input 태그 중 활성화된 객체 선택 - 배경색상 : lavender
+    $('#div3 :enabled').css('background-color', 'lavender');
+
+    // 2) input 태그 중 비활성화된 객체 선택 - 배경색상 : lightcyan
+    $('#div3 :disabled').css('background-color', 'lightcyan');
+
+    // 3) input 태그 중 체크된 객체 선택 - 사이즈 : 30px X 30px
+    $('#div3 :checked').css({
+        'width': '30px', 'height': '30px'
+    });
+
+    // 4) checkbox에 change 이벤트 핸들러 등록
+    $('#div3 :checkbox').change(function(){
+        console.log(this);
+        console.log($(this));
+
+        const checkbox = $(this);
+        console.log(checkbox.attr('checked'));
+        console.log(checkbox.prop('checked'));
+
+        // 체크가 되면 사이즈를 50px 50px
+        // 체크가 안되면 사이즈를 15px 15px
+        if(checkbox.prop('checked')){
+            checkbox.css({'width': '50px', 'height': '50px'});
+        }else {
+            checkbox.css({'width': '15px', 'height': '15px'})
+        }
+    })
+
+    // 5) select에 change 이벤트 핸들러 등록
+    // opition 태그 중 선택된 값이 각각 result1에는 국가명, result2에는 취미 들어가게
+
+    $('#national').change(function(){
+        // select 태그의 option 태그 중 선택된 객체
+        const value = $('#national option:selected').val();
+        $('#result1').val(value);
+
+    });
+
+    $('#hobby').change(function(){
+        const value = $('#hobby option:selected').val();
+        $('#result2').val(value);
+    });
+
+    // 8. attr()와 prop()
+    let cb1 = $('#cb1');
+    let cb2 = $('#cb2');
+
+    // attr()로 checked 값 읽어오기
+    console.log(`cb1.attr('checked') : ${cb1.attr('checked')}`);
+    console.log(`cb2.attr('checked') : ${cb2.attr('checked')}`);
+
+    // prop()로 checked 값 읽어오기
+    console.log(`cb1.prop('checked') : ${cb1.prop('checked')}`);
+    console.log(`cb2.prop('checked') : ${cb2.prop('checked')}`);
+
+    // attr()와 prop()로 값 설정하기
+    cb2.attr('checked', 'checked');
+    cb1.prop('checked', false);
+});
---
+++ 4_jQuery/js/02_객체탐색.js
@@ -0,0 +1,103 @@
+// 1. Filtering 메소드
+// 선택된 요소들 중 첫번째 요소를 가져와서 스타일 변경
+$('h4').first().css("font-size", "4rem");
+$('h4:first').css("font-size", "3rem");
+$('h4').filter(':first').css("font-size", "2rem");
+
+// h4 중 마지막 요소의 글자 크기를 2rem으로 
+$('h4').last().css("font-size", "4rem");
+$('h4:last').css("font-size", "4rem");
+$('h4').filter(':last').css("font-size", "4rem");
+
+// h4 중 다섯번쨰 요소의 텍스트를 '테스트-5'로 변경
+// 인덱스니까 4!
+$('h4').eq(4).text("테스트-5");
+
+// 클래스 선택자 : test를 선택해서 홀수 인덱스 요소만 배경색상 : royalblue, 글자색 : white
+$('.test').odd().css({'background-color':'royalblue', 'color': 'white'});
+
+// 클래스 선택자 : test를 선택해서 짝수 인덱스 요소만 배경색상 : hotpink, 글자색 : white
+$('.test').even().css({'background-color':'hotpink', 'color': 'white'});
+
+// h4 중 클래스 선택자 test가 아닌 요소를 선택해서 배경색상 : chocolate, 글자색 : white
+$('h4').not('.test').css({'background-color':'chocolate', 'color': 'white'});
+
+// 선택된 요소들 중 특정 요소가 존재하는지 확인(있으면 true, 없으면 false)
+console.log($('h4').is('.test'));
+console.log($('h4').is('.test1'));
+
+// 2. Ancestors & Descendants 메소드
+// 선택된 요소의 바로 상위에 있는 부모 요소
+console.log($('span').parent()); // li, p
+
+// 선택된 요소의 모든 상위 요소
+console.log($('span').parents());
+
+// span 요소의 부모 요소에 스타일
+// 글자색 : red, border : 2px solid
+$('span').parent().css({
+    'color': 'red', 'border': '2px solid'
+});
+// li 요소의 모든 상위 요소에 스타일 적용
+// 글자색 : bule
+$('li').parents().css("color","blue");
+
+// li 요소의 상위 요소 중 div만 스타일 적용
+// border : 굵기 - 2px, 스타일 - dashed, 색상 : tomato
+$('li').parents('div').css('border','2px dashed tomato');
+
+// span 요소부터 div 요소 이전까지 상위 요소 스타일 적용
+// 배경색 : lavenderblush
+$('span').parentsUntil('div').css("background-color","lavenderblush");
+
+// 별도로 스타일 작성
+let style1 = {'color':'deeppink', 'border':'2px solid'};
+let style2 = {'color':'blue', 'border':'2px solid'};
+let style3 = {'color':'green', 'border':'2px solid'};
+let style4 = {'color':'purple', 'border':'2px solid'};
+let style5 = {'color':'orange', 'border':'2px solid'};
+
+console.log($('.wrap').children());
+console.log($('.wrap').find('span'));
+
+// .wrap인 요소의 자식 요소들에게 style1을 적용
+$('.wrap').children().css(style1);
+
+// .wrap인 요소의 자식의 자식요소들에 style2를 적용
+$('.wrap').children().children().css(style2);
+
+// .wrap인 요소의 자식의 자식 요소 중에 ul 태그의 style3을 적용
+$('.wrap').children().children('ul').css(style3);
+
+// .wrap인 요소의 자식의 자식의 자식 요소 중에 li 태그의 style4를 적용
+$('.wrap').children().children().children('li').css(style4);
+
+// .wrap인 요소의 모든 하위 요소 중 sapn 태그의 style5를 적용
+$('.wrap').find('span').css(style5);
+
+// 4. Sideways 메소드
+console.log($('.wrap2 h2').siblings());
+
+// h2 요소의 형제 요소들에 style3 적용
+$('h2').siblings().css(style3);
+
+// h2 요소의 형제 요소 중 p 요소에 style2 적용
+$('h2').siblings('p').css(style2);
+
+// h2 요소의 다음 형제 요소에 style1 적용
+$('h2').next().css(style1);
+
+// h2 요소의 다음에 오는 모든 형제 요소에 배경색: pink
+$('h2').next().nextAll().css('background-color','pink');
+
+// h2 요소 이후부터 p요소 이전에 모든 형제 요소에 테두리 스타일 : dashed
+$('h2').next().nextUntil().css('border','dashed');
+
+// h2 요소의 이전 형제 요소에 style5 적용
+$('h2').prev().css(style5);
+
+// h2 요소의 이전에 오는 모든 형제 요소에 배경새 : moccasin
+$('h2').prevAll().css('background-color','moccasin');
+
+// h2 요소 이전부터 p 요소 이후까지의 모든 형제 요소에 글자 크기 : 2rem
+$('h2').prevUntill('p').css('font-size', '2rem');
